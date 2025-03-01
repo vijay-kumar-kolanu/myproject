c660e3d5ece440177fbb164431bcf38581ce7ca9
vijay-kumar-kolanu <vijaykumarkolanu20@gmail.com>
2025-03-01 18:21:14 +0530

    Add files via upload

---
+++ Tokens/Primitive_Variable.java
@@ -0,0 +1,29 @@
+package Tokens;
+
+public class Primitive_Variable {
+public static void main(String[] args) {
+	byte a=20;
+	short b=30;
+	int c=40;
+	long d=9346834054l;
+	float e=4.555555555f;
+	double f=3.0;
+	char g='h';
+	boolean h=true;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.err.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	System.out.println(h);
+	
+	
+	System.out.println("***************************");
+	long z=9346834054l;
+	float x=2.5555555f;
+	System.out.println(z);
+System.out.println(x);
+}
+}
